5e4ffb655e1628c3198a40c1be363ae2a79bc061
BanriYasui <dontstop1110@gmail.com>
2022-02-13 16:39:08 +0900

    MyBatis基本構文
    if, where, foreach, <, >

--- src/test/java/com/example/springmybatis/da/mapper/InquiryTagMapperTest.java
+++ src/test/java/com/example/springmybatis/da/mapper/InquiryTagMapperTest.java
@@ -3,11 +3,16 @@ package com.example.springmybatis.da.mapper;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.apache.ibatis.session.RowBounds;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.example.springmybatis.da.query.InquiryTagSelectQuery;
+import com.example.springmybatis.da.query.InquiryTagSelectQuery.OrderByField;
+import com.example.springmybatis.da.query.InquiryTagSelectQuery.SortOrder;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -30,20 +35,20 @@ class InquiryTagMapperTest {
     @Test
     void select() {
         // TODO ハンズオン 2-3 コメントアウトを解除してテストを実行
-//        var query = new InquiryTagSelectQuery();
-//
-//        query.setDescription("問合せ");
-//
-//        query.addOrderCondition(OrderByField.INQUIRY_ID, SortOrder.DESC);
-//        query.addOrderCondition(OrderByField.ID, SortOrder.ASC);
-//
-//        var list = inquiryTagMapper.select(query, new RowBounds(0, 100));
-//
-//        assertEquals(2, list.size());
-//
-//        var inquiryTag = list.get(0);
-//
-//        assertEquals(7, inquiryTag.getId());
+        var query = new InquiryTagSelectQuery();
+
+        query.setDescription("問合せ");
+
+        query.addOrderCondition(OrderByField.INQUIRY_ID, SortOrder.DESC);
+        query.addOrderCondition(OrderByField.ID, SortOrder.ASC);
+
+        var list = inquiryTagMapper.select(query, new RowBounds(0, 100));
+
+        assertEquals(2, list.size());
+
+        var inquiryTag = list.get(0);
+
+        assertEquals(7, inquiryTag.getId());
     }
 
 
