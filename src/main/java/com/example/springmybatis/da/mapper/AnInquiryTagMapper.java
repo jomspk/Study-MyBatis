7f5debb59f337869a8fcb8aff09483994656c7d7
BanriYasui <dontstop1110@gmail.com>
2022-02-19 19:07:58 +0900

    learned typeHandler

--- src/main/java/com/example/springmybatis/da/mapper/AnInquiryTagMapper.java
+++ src/main/java/com/example/springmybatis/da/mapper/AnInquiryTagMapper.java
@@ -1,6 +1,10 @@
 package com.example.springmybatis.da.mapper;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 import com.example.springmybatis.da.entity.InquiryTag;
@@ -10,15 +14,31 @@ import com.example.springmybatis.da.entity.InquiryTag;
 public interface AnInquiryTagMapper {
 
     // TODO ハンズオン 8-1-1 find用のアノテーションを定義
+	@Select({"		select id, inquiry_id, description, created", 
+		"		from inquiry_tag", 
+		"		where id = #{id}"})
     InquiryTag find(int id);
 
     // TODO ハンズオン 8-1-2 insert用のアノテーションを定義
+	@Insert({"    	insert into inquiry_tag(inquiry_id, description, created)", 
+	"    	values (#{inquiryId},#{description},#{created})"})
     void insert(InquiryTag inquiryTag);
 
     // TODO ハンズオン 8-1-3 update用のアノテーションを定義
+	@Update({"<script>",
+		"    	update inquiry_tag", 
+			"    	<set>", 
+			"    		inquiry_id = #{inquiryId},", 
+			"    		description = #{description},", 
+			"    		created = #{created},",
+			"    	</set>",
+			"    	where id = #{id}",
+			"</script>"})
     void update(InquiryTag inquiryTag);
 
     // TODO ハンズオン 8-1-4 delete用のアノテーションを定義
+	@Delete({"    	delete from inquiry_tag", 
+	"    	where id = #{id}"})
 	void delete(int id);
 
 }
